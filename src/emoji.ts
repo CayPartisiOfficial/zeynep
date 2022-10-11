768a2a8a2a992b82b422512688354ccca352d830
Karahan Buhan <root@karahanbuhan.com>
2022-10-11 22:28:13 +0300

    Add boilerplate for emoji slash command

---
+++ src/emoji.ts
@@ -0,0 +1,7 @@
+import { CommandInteraction } from "discord.js";
+
+export const emoji = { name: "emoji", description: "Emoji menüsünü açar" };
+
+export async function execute(interaction: CommandInteraction) {
+    await interaction.reply("todo!");
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,33 @@
 require("dotenv").config();
 
-import { Client, GatewayIntentBits } from "discord.js"
+import { Client, GatewayIntentBits, REST, Routes } from "discord.js";
+import { emoji, execute as executeEmoji } from "./emoji";
 
-const { Guilds, MessageContent, GuildMessages, GuildMembers } = GatewayIntentBits
-const client = new Client({ intents: [Guilds, MessageContent, GuildMessages, GuildMembers] })
+// Reload and update slash commands
+(async () => {
+    const rest = new REST({ version: "10" }).setToken(process.env.TOKEN!);
+    try {
+        console.log('Started refreshing application (/) commands.');
 
-client.login(process.env.TOKEN)
+        await rest.put(Routes.applicationCommands("1029453218465456270"), { body: [emoji] });
+
+        console.log('Successfully reloaded application (/) commands.');
+    } catch (error) {
+        console.error(error);
+    }
+})();
+
+const { Guilds, MessageContent, GuildMessages, GuildMembers } = GatewayIntentBits;
+const client = new Client({ intents: [Guilds, MessageContent, GuildMessages, GuildMembers] });
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user!.tag}`);
+});
+
+client.on("interactionCreate", async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+
+    if (interaction.commandName === "emoji") await executeEmoji(interaction);
+});
+
+client.login(process.env.TOKEN);
