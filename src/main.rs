f42d73c6954fa20bd7c255ddcff79f52ec38296a
Karahan Buhan <root@karahanbuhan.com>
2022-11-14 19:37:00 +0300

    Add colour of hour feature which changes color of member role every hour to something random

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
+use std::time::Duration;
+
+use rand::Rng;
 use serenity::async_trait;
 use serenity::client::ClientBuilder;
 use serenity::model::application::interaction::{Interaction, InteractionResponseType};
 use serenity::model::gateway::{GatewayIntents, Ready};
 use serenity::model::prelude::command::Command;
+use serenity::model::prelude::{GuildId, RoleId};
 use serenity::prelude::{Context, EventHandler};
+use tokio::{task, time};
 use tracing::{error, info};
 
 mod commands;
@@ -15,11 +20,17 @@ impl EventHandler for Handler {
     async fn ready(&self, ctx: Context, event: Ready) {
         info!("Logged as {}", event.user.name);
 
-        /*let guild_id = GuildId(
+        let guild_id = GuildId(
             env!("GUILD_ID", "Expected GUILD_ID in environment")
                 .parse()
-                .expect("GUILD_ID must be a number!"),
-        );*/
+                .expect("GUILD_ID must be a number"),
+        );
+
+        let role_id = RoleId(
+            env!("ROLE_ID", "Expected ROLE_ID in environment")
+                .parse()
+                .expect("ROLE_ID must be a number"),
+        );
 
         let _commands = Command::create_global_application_command(&ctx.http, |command| {
             commands::ping::register(command)
@@ -28,6 +39,30 @@ impl EventHandler for Handler {
         .expect("Could not register commands!");
 
         info!("Slash commands are registered");
+
+        // Update member role colour every hour to something random
+        task::spawn(async move {
+            let mut interval = time::interval(Duration::from_secs(3600));
+            interval.tick().await; // First tick completes immediately
+
+            loop {
+                interval.tick().await;
+
+                let result = GuildId::edit_role(guild_id, &ctx.http, &role_id, |role| {
+                    role.colour(rand::thread_rng().gen_range(0..16777215))
+                })
+                .await;
+
+                match result {
+                    Ok(_) => {
+                        info!("Successfully changed the colour of hour")
+                    }
+                    Err(err) => {
+                        error!("Could not change colour of hour: {}", err)
+                    }
+                }
+            }
+        });
     }
 
     async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
