2b9aecce606da6ee41bb641c30658a9fe80b0ac5
Karahan Buhan <root@karahanbuhan.com>
2022-11-17 19:47:19 +0300

    Refactor main.rs

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,1 +1,20 @@
+use std::sync::Arc;
+
+use anyhow::Context;
+use serenity::http::Http;
+use serenity::model::prelude::command::Command;
+use tracing::info;
+
+use crate::ResultTraceErr;
+
 pub mod ping;
+
+pub async fn register_slash_commands(http: Arc<Http>) {
+    Command::create_global_application_command(http, |command| ping::register(command))
+        .await
+        .context("Could not register commands!")
+        .trace_err()
+        .ok();
+
+    info!("Slash commands are registered");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
-use anyhow::Result;
-use serenity::async_trait;
+use anyhow::{Context, Result};
 use serenity::client::ClientBuilder;
 use serenity::model::application::interaction::{Interaction, InteractionResponseType};
 use serenity::model::gateway::{GatewayIntents, Ready};
-use serenity::model::prelude::command::Command;
 use serenity::model::prelude::{GuildId, RoleId};
-use serenity::prelude::{Context, EventHandler};
+use serenity::prelude::EventHandler;
+use serenity::{async_trait, prelude};
 use tracing::{error, info};
 
 use crate::cotd::Cotd;
@@ -27,36 +26,21 @@ impl<T> ResultTraceErr for Result<T> {
     }
 }
 
-struct Handler {}
+struct Handler {
+    guild_id: GuildId,
+    role_id: RoleId,
+}
 
 #[async_trait]
 impl EventHandler for Handler {
-    async fn ready(&self, ctx: Context, event: Ready) {
+    async fn ready(&self, ctx: prelude::Context, event: Ready) {
         info!("Logged as {}", event.user.name);
 
-        let guild_id = GuildId(
-            env!("GUILD_ID", "Expected GUILD_ID in environment")
-                .parse()
-                .expect("GUILD_ID must be a number"),
-        );
-
-        let role_id = RoleId(
-            env!("ROLE_ID", "Expected ROLE_ID in environment")
-                .parse()
-                .expect("ROLE_ID must be a number"),
-        );
-
-        Cotd::start(ctx.http.clone(), guild_id, role_id).await;
-
-        let _commands = Command::create_global_application_command(&ctx.http, |command| {
-            commands::ping::register(command)
-        })
-        .await
-        .expect("Could not register commands!");
-        info!("Slash commands are registered");
+        commands::register_slash_commands(ctx.http.clone()).await;
+        Cotd::start(ctx.http, self.guild_id, self.role_id).await;
     }
 
-    async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
+    async fn interaction_create(&self, ctx: prelude::Context, interaction: Interaction) {
         if let Interaction::ApplicationCommand(command) = interaction {
             info!(
                 "Received command interaction {} by {}",
@@ -68,28 +52,19 @@ impl EventHandler for Handler {
                 _ => String::from("Böyle bir özelliğim yok :("),
             };
 
-            let result = command
+            command
                 .create_interaction_response(&ctx.http, |response| {
                     response
                         .kind(InteractionResponseType::ChannelMessageWithSource)
                         .interaction_response_data(|message| message.content(content))
                 })
-                .await;
-
-            match result {
-                Ok(_) => {
-                    info!(
-                        "Successfully finished command interaction {} by {}",
-                        command.data.name, command.user
-                    )
-                }
-                Err(_) => {
-                    error!(
-                        "Error occurred before finishing command interaction {} by {}",
-                        command.data.name, command.user
-                    )
-                }
-            }
+                .await
+                .context(format!(
+                    "Error occurred before finishing command interaction {} by {}",
+                    command.data.name, command.user
+                ))
+                .trace_err()
+                .ok();
         }
     }
 }
@@ -99,13 +74,24 @@ async fn main() {
     tracing_subscriber::fmt::init();
 
     let token = env!("DISCORD_TOKEN", "Expected DISCORD_TOKEN in the environment");
+    let guild_id = GuildId(
+        env!("GUILD_ID", "Expected GUILD_ID in environment")
+            .parse()
+            .expect("GUILD_ID must be a number"),
+    );
+
+    let role_id = RoleId(
+        env!("ROLE_ID", "Expected ROLE_ID in environment")
+            .parse()
+            .expect("ROLE_ID must be a number"),
+    );
+
     let intents = GatewayIntents::GUILD_MESSAGES
         | GatewayIntents::DIRECT_MESSAGES
         | GatewayIntents::MESSAGE_CONTENT;
     let mut client = ClientBuilder::new(token, intents)
-        .event_handler(Handler {})
+        .event_handler(Handler { guild_id, role_id })
         .await
         .expect("Error logging in");
-
     client.start().await.expect("Error starting client");
 }
