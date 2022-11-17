b20453a7e2c9a314f79b61737078123bed144859
Karahan Buhan <root@karahanbuhan.com>
2022-11-17 19:51:53 +0300

    More refactoring in cotd.rs

--- src/cotd.rs
+++ src/cotd.rs
@@ -13,52 +13,49 @@ use tracing::info;
 
 use crate::ResultTraceErr;
 
-/// COTD stands for Colour of the Day, this struct implements changing colour of member role every day
-pub struct Cotd {
-    colour: Colour,
-    last_change: DayOfMonth,
+pub async fn start(http: Arc<Http>, guild_id: GuildId, role_id: RoleId) {
+    let mut colour = generate_random_colour();
+    let mut last_change = DayOfMonth::from_file_or_now(".cotd").await;
+
+    tokio::task::spawn(async move {
+        info!("Colour of the Day is started");
+
+        let mut interval = tokio::time::interval(Duration::from_secs(600));
+        loop {
+            interval.tick().await;
+
+            let now = DayOfMonth::now().await;
+            // Continue if it is the same day
+            if now == last_change {
+                continue;
+            }
+
+            colour = generate_random_colour();
+
+            if guild_id
+                .edit_role(&http, role_id, |role| role.colour(colour.0 as u64))
+                .await
+                .context("Cannot edit role")
+                .trace_err()
+                .is_ok()
+            {
+                last_change = now;
+                last_change.save_to_file(".cotd").await.trace_err().ok();
+            }
+
+            info!("Updated colour of the day to 0x{}", colour.hex());
+        }
+    });
 }
 
-impl Cotd {
-    pub async fn start(http: Arc<Http>, guild_id: GuildId, role_id: RoleId) {
-        let mut cotd = Cotd {
-            colour: generate_random_colour(),
-            last_change: DayOfMonth::from_file_or_now(".cotd").await,
-        };
+fn generate_random_colour() -> Colour {
+    let colour = Colour(rand::thread_rng().gen_range(0..16777216));
+    let lum = 0.299 * colour.r() as f32 + 0.587 * colour.g() as f32 + 0.114 * colour.b() as f32;
 
-        tokio::task::spawn(async move {
-            info!("Colour of the Day is started");
-
-            let mut interval = tokio::time::interval(Duration::from_secs(600));
-            loop {
-                interval.tick().await;
-
-                let now = DayOfMonth::now().await;
-                // Continue if it is the same day
-                if now == cotd.last_change {
-                    continue;
-                }
-
-                cotd.colour = generate_random_colour();
-
-                if guild_id
-                    .edit_role(&http, role_id, |role| role.colour(cotd.colour.0 as u64))
-                    .await
-                    .context("Cannot edit role")
-                    .trace_err()
-                    .is_ok()
-                {
-                    cotd.last_change = now;
-                    cotd.last_change
-                        .save_to_file(".cotd")
-                        .await
-                        .trace_err()
-                        .ok();
-                }
-
-                info!("Updated colour of the day to 0x{}", cotd.colour.hex());
-            }
-        });
+    if lum > 150.0 {
+        colour
+    } else {
+        generate_random_colour()
     }
 }
 
@@ -94,16 +91,5 @@ impl DayOfMonth {
 impl PartialEq<Self> for DayOfMonth {
     fn eq(&self, other: &Self) -> bool {
         self.0 == other.0
-    }
-}
-
-fn generate_random_colour() -> Colour {
-    let colour = Colour(rand::thread_rng().gen_range(0..16777216));
-    let lum = 0.299 * colour.r() as f32 + 0.587 * colour.g() as f32 + 0.114 * colour.b() as f32;
-
-    if lum > 150.0 {
-        colour
-    } else {
-        generate_random_colour()
     }
 }
